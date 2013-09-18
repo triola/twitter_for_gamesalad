813df5c96362734bad8854b42b37d2d6c9db9060
Ben Triola <ben@therampant.com>
2013-09-17 20:16:20 -0500

    generic PHP script for user modification

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+ 
+$mobile_browser = '0';
+ 
+if (preg_match('/(up.browser|up.link|mmp|symbian|smartphone|midp|wap|phone|android)/i', strtolower($_SERVER['HTTP_USER_AGENT']))) {
+    $mobile_browser++;
+}
+ 
+if ((strpos(strtolower($_SERVER['HTTP_ACCEPT']),'application/vnd.wap.xhtml+xml') > 0) or ((isset($_SERVER['HTTP_X_WAP_PROFILE']) or isset($_SERVER['HTTP_PROFILE'])))) {
+    $mobile_browser++;
+}    
+ 
+$mobile_ua = strtolower(substr($_SERVER['HTTP_USER_AGENT'], 0, 4));
+$mobile_agents = array(
+    'w3c ','acs-','alav','alca','amoi','audi','avan','benq','bird','blac',
+    'blaz','brew','cell','cldc','cmd-','dang','doco','eric','hipt','inno',
+    'ipaq','java','jigs','kddi','keji','leno','lg-c','lg-d','lg-g','lge-',
+    'maui','maxo','midp','mits','mmef','mobi','mot-','moto','mwbp','nec-',
+    'newt','noki','oper','palm','pana','pant','phil','play','port','prox',
+    'qwap','sage','sams','sany','sch-','sec-','send','seri','sgh-','shar',
+    'sie-','siem','smal','smar','sony','sph-','symb','t-mo','teli','tim-',
+    'tosh','tsm-','upg1','upsi','vk-v','voda','wap-','wapa','wapi','wapp',
+    'wapr','webc','winw','winw','xda ','xda-');
+ 
+if (in_array($mobile_ua,$mobile_agents)) {
+    $mobile_browser++;
+}
+ 
+if (strpos(strtolower($_SERVER['ALL_HTTP']),'OperaMini') > 0) {
+    $mobile_browser++;
+}
+ 
+if (strpos(strtolower($_SERVER['HTTP_USER_AGENT']),'windows') > 0) {
+    $mobile_browser = 0;
+}
+ 
+if ($mobile_browser > 0) {
+//This opens a user profile on the user's twitter app. Replace 1234 with your twitter user ID which you can get here: http://gettwitterid.com/
+  header('Location: twitter://user?id=1234');
+
+}
+else {
+//This opens the twitter profile in the browser on non-mobile devices
+  header('Location: https://twitter.com/happybadgers');
+
+}   
+ 
+?>
